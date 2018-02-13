6fff221d59fbf3d85ffff79c1e956358e239ea3c
Charlotte Joy Callender <charlottejoyliving@gmail.com>
2018-02-12 20:14:57 -0500

    Nav with JS, collapse on click. Add side images, spacey

--- responsive-layout-conquer/js/index.js
+++ responsive-layout-conquer/js/index.js
@@ -1,1 +1,9 @@
-//media query
+//media query 
+//media query 
+$(".hamburger").click(function(){
+ $(".menu").toggleClass("menu--open");
+});
+
+$(".menu-link").click(function(){
+  $(".menu").removeClass("menu--open");
+})
